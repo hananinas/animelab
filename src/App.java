2266046a6b96ac57610e99731fc5f8333a9628e3
hananinas <hanani.nas@gmail.com>
2022-11-24 12:42:11 +0100

    init commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    private Parent hello(){
+        
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.show();
+
+    }
+}
