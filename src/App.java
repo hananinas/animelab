e429c0406c635e1759590f26e7e107f9b4e36b22
hananinas <hanani.nas@gmail.com>
2022-11-26 01:17:22 +0100

    testing

--- src/App.java
+++ src/App.java
@@ -1,20 +1,24 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
-
-    private Parent hello(){
-        
-    }
-
+    
     @Override
     public void start(Stage stage) throws Exception {
-
-        stage.show();
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
